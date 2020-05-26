5fe3517a85aed14d9f17bb814a9cafe0672d481f
dphuyijin <dphuyijin@dfl.com.cn>
2020-05-26 12:48:44 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: 'production',
+  entry: __dirname + '/src/calendar-utils.ts',
+  output: {
+    path: __dirname + '/dist/bundles',
+    filename: 'calendar-utils.umd.js',
+    libraryTarget: 'umd',
+    library: 'calendarUtils',
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          compilerOptions: {
+            module: 'es2015',
+            declaration: false,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
